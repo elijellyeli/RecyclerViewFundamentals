dc7adc3b8519b3097326c2182266b0fe1654305f
Eli Hirsch <elijellyeli@gmail.com>
2020-01-17 17:15:13 +0200

    Added data binding to RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -113,11 +113,9 @@ class SleepTrackerFragment : Fragment() {
 
         // Observe nights for RecyclerView
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-            sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
-                it?.let {
-                    adapter.data = it
-                }
-            })
+            it?.let {
+                adapter.submitList(it)
+            }
         })
 
         return binding.root
